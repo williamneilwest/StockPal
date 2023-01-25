afa4135863701194a1f232acd3911b487573c948
William West <wharris1597@gmail.com>
2023-01-25 00:08:45 -0700

    edited database methods

--- app/src/main/java/com/example/stockpal/MainActivity.java
+++ app/src/main/java/com/example/stockpal/MainActivity.java
@@ -2,23 +2,35 @@ package com.example.stockpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "Lifecycle";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate");}
+        Log.d(TAG, "onCreate");
+
+
+
+    }
 
 
 
@@ -58,4 +70,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG, "onResume");
     }
+
+
+
 }
--- app/src/main/java/com/example/stockpal/SignupPage.java
+++ app/src/main/java/com/example/stockpal/SignupPage.java
@@ -3,12 +3,55 @@ package com.example.stockpal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupPage extends AppCompatActivity {
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd, btnViewData, btnDelete;
+    private EditText editText, editText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+
+        editText = (EditText) findViewById(R.id.nameFirstField);
+        editText2 = (EditText) findViewById(R.id.nameLastField);
+
+        btnAdd = (Button) findViewById(R.id.createAccount);
+
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                }
+                else{
+                    toastMessage("You must enter something into the field!");
+                }
+            }
+        });
+    }
+    public void AddData(String newEntry){
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+        if(insertData) {
+            toastMessage("Data Successfully Inserted!");
+        }
+        else {
+            toastMessage("Something went wrong!");
+        }
+
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
 }
